6322a61dc4cf1b264ce09495713648bb59a52f07
QKJIN <85993064+QKJIN@users.noreply.github.com>
2024-09-06 17:07:07 +0800

    Update index.js
    change to the domain owned

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.pyapi3380.top": dockerHub,
+  "quay.pyapi3380.top": "https://quay.io",
+  "gcr.pyapi3380.top": "https://gcr.io",
+  "k8s-gcr.pyapi3380.top": "https://k8s.gcr.io",
+  "k8s.pyapi3380.top": "https://registry.k8s.io",
+  "ghcr.pyapi3380.top": "https://ghcr.io",
+  "cloudsmith.pyapi3380.top": "https://docker.cloudsmith.io",
+  "ecr.pyapi3380.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.pyapi3380.top": dockerHub,
 };
 
 function routeByHosts(host) {
